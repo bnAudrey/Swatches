7dfd1bde586517e26a128063ca4e2efbd10d4fe8
bnAudrey <brigitta.nataudrey@gmail.com>
2022-07-22 20:57:50 +0700

    Initial Commit Title, Next Button, Collection View Cell Trial

---
+++ Swatches/SwatchCategoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SwatchCategoryCollectionViewCell.swift
+//  Swatches
+//
+//  Created by Brigitta Audrey on 22/07/22.
+//
+
+import UIKit
+
+class SwatchCategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
