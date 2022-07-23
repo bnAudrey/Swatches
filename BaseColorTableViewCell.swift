58dded0c165861e1f80871518bf1763403b1a5a5
bnAudrey <brigitta.nataudrey@gmail.com>
2022-07-23 16:36:31 +0700

    Finishing Collection View Cell Base Color Table View Cell

---
+++ BaseColorTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BaseColorTableViewCell.swift
+//  Swatches
+//
+//  Created by Brigitta Audrey on 23/07/22.
+//
+
+import UIKit
+
+class BaseColorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
