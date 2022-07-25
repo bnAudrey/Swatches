6f602e821dd3f22399017ca7de97dc6076802852
bnAudrey <brigitta.nataudrey@gmail.com>
2022-07-25 21:46:31 +0700

    Color Combination Table View + 5 color watches

---
+++ Swatches/ColorCombinationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ColorCombinationTableViewCell.swift
+//  Swatches
+//
+//  Created by Brigitta Audrey on 25/07/22.
+//
+
+import UIKit
+
+class ColorCombinationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Swatches/Model/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  Swatches Data
+//
+//  Created by Gregorius Albert on 25/07/22.
+//
+
+import Foundation
+
+struct Color {
+    var colorName:String = ""
+    var colorTag:String = ""
+    var hexValue:String = ""
+    var red:Int = 0
+    var green:Int = 0
+    var blue:Int = 0
+}
